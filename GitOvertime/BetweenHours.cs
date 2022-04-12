4c0d956151cbde2d0280f7d871d5cc5e6789f782
iAm9001 <iam9001@outlook.com>
2022-04-12 10:57:00 -0400

    Initial Commit - Add gitignore - Proof of concept loops

---
+++ GitOvertime/BetweenHours.cs
@@ -0,0 +1,22 @@
+﻿using LibGit2Sharp;
+
+namespace GitOvertime;
+
+public class BetweenHours
+{
+    public void GetCommitsBetweenHours(string repoPath, int h1, int h2)
+    {
+        using (var repo = new Repository(repoPath))
+        {
+            var afterHoursOnly =
+                repo.Commits.Where(commit => commit.Committer.When.Hour < h1 || commit.Committer.When.Hour > h2);
+
+            foreach (var c in afterHoursOnly)
+            {
+                Console.WriteLine($"{repo.Branches.Where(b => b.Commits.Contains(c)).FirstOrDefault().FriendlyName}{c.Author.Email} - {c.Author.Name} - {c.Committer.When.ToString()}" +
+                                  $"\r\nID: {c.Id.Sha}: {c.Message}");
+            }
+            
+        }
+    }
+}
---
+++ GitOvertime/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GitOvertime;
+
+Console.WriteLine("Hello, World!");
+
+var gitter = new BetweenHours();
+gitter.GetCommitsBetweenHours(@"C:\Users\brand\source\repos\Sequence Diagram Generator", 8, 16);
